ecc5359d6276fa994740e6e5772f860e6ce61e67
chester669 <viktorostash44@gmail.com>
2024-04-19 15:32:59 +0300

    TEST

---
+++ js/task-1.js
@@ -0,0 +1,22 @@
+const pricePerDroid = 300;
+const quantity = 4;
+const totalPrice = pricePerDroid * quantity;
+const makeTransition = `You ordered ${quantity} drones worth ${totalPrice} credits!`;
+console.log(makeTransition);
+
+// const fruit = "mango"
+//  let fruit = "mango"
+// console.log(fruit)
+//const isModalOpen = true
+// const isAdmin = false
+// null
+// undefinded
+// typeof fruit = string
+// let a = 5 . let b = 2 . a += 2 (a=7) . b -= 1 (b = 1). a *= 3 (a = 15)
+// string(5) = "5"
+// const fruit = "mango"; const weight = 20 ; const price = `your ${fruit} weight ${weight} so it cost ${totaPrice};
+// const a = 2; const b = 4; console.log(a > b) - false; console.log(b >= a) - true;
+//console.log(5 === 5) - true; console.log(5 !== 5) - false; console.log(5 !== "5") - true;
+//const order = fruit === fruit;
+//console.log(Number("5")) - 5; console.log(Number(false)) - 0; console.log(Number(true)) - 1;
+//console.log("5" * 2) - 10; console.log(5  + true) - 6
