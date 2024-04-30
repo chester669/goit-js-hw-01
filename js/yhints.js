cfd42df1c012b954e8f324ea53ea0cbf000b6625
chester669 <viktorostash44@gmail.com>
2024-04-30 14:18:40 +0300

    added some hints

--- js/yhints.js
+++ js/yhints.js
@@ -67,3 +67,187 @@
 // }
 // const message = full("radar", 3000);
 // console.log(message);
+
+////////////////////////////////////////////// if, switch, && , ||, ! - operators ///////////////////////////////////////////////
+
+// let price = 100;
+// subscription = "pro";
+// if (subcription === "pro")  {
+// price = 100; };
+
+//let price = 0;
+//subscription = "free";
+//if ( subsription === "pro") {
+//price = 100; }
+
+// function checkAge(age) {
+//   if (age >= 18) {
+//     return (checkAge = "You are an adult");
+//   }
+// }
+// console.log(checkAge(20));
+
+// let age = 20;
+
+// if (age >= 18) {
+//   age = "you are an adult";
+// }
+// console.log(age);
+
+//  /////////////////////////// else ////////////////////////////////
+
+// function checkStorage(available, ordered) {
+//   if (available > ordered) {
+//     return (checkStorage = "Order is processed, our manager will contact you");
+//   }
+// }
+// console.log(checkStorage(20, 10));
+
+// /////////////////////// else if //////////////////////////////////
+
+// function checkStorage(available, ordered) {
+//   if (ordered === 0) {
+//     return (checkStorage = "There are no products in the order!");
+//   } else if (available < ordered) {
+//     return (checkStorage =
+//       "Your order is too large, there are not enough items in stock!");
+//   } else available > ordered;
+//   {
+//     return (checkStorage =
+//       "The order is accepted, our manager will contact you");
+//   }
+// }
+
+// //////////////// тернарний оперант ( conditional / ternary operant) ////////////////////
+
+// function checkPassword(password) {
+//   const correctPassword = "jqueryismyjam";
+//   return (checkPassword =
+//     password === correctPassword
+//       ? "Access granted"
+//       : "Access denied, wrong password!");
+// }
+
+// function getSubscriptionPrice(type) {
+//   switch (type) {
+//     case "starter":
+//       return 0;
+//       break;
+//     case "professional":
+//       return 20;
+//       break;
+//     case "organization":
+//       return 50;
+//       break;
+//     default:
+//       return "there are not such a subscription";
+//   }
+// }
+// console.log(getSubscriptionPrice("organization"));
+
+///////////////////////////////////////////  logic operant (&&, ||, !) //////////////////////////////////////////////////////
+
+// ////////////////////////// && ////////////////////////////
+// function isNumberInRange(start, end, number) {
+//   const numberInclude = start <= number && end >= number;
+//   if (start <= number && end >= number) {
+//     return true;
+//   } else start >= number && end <= number;
+//   {
+//     return false;
+//   }
+//   return numberInclude;
+// }
+// console.log(isNumberInRange(10, 30, 20));
+
+// //////////////////////// || ////////////////////////////////
+
+// function checkAccess(subType) {
+//   const subscription = subType === "pro" || subType === "vip";
+//   return subscription;
+//   if ("pro" === subType || "vip" === subType) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+// console.log(checkAccess("pro"));
+
+//////////////////////////// ! ///////////////////////////////
+
+// function toggleModalVisibility(isVisible) {
+//   const visible = !isVisible;
+//   return visible;
+// }
+// console.log(toggleModalVisibility(true));
+
+// ////////////////////////////////////////// methods (slice) /////////////////////////////////////////////////
+
+// function getSubstring(string, length) {
+//   const substring = string.slice(0, length);
+//   return substring;
+// }
+
+// console.log(getSubstring("hello babe", 4));
+
+/////////////////////////// toUpperCase toLowerCase ///////////////////////
+
+// function normalizeInput(input, to) {
+//   if (to === "lower") {
+//     return input.toLowerCase();
+//   } else if (to === "upper") {
+//     return input.toUpperCase();
+//   } else {
+//     return input;
+//   }
+// }
+// console.log(normalizeInput("HellO My NAMe Is VIktor", "lower"));
+
+////////////////////////////////// includes ////////////////////////////
+
+// function checkBoolean(right, wrong) {
+//   let message = right === wrong;
+//   return message;
+// }
+
+// console.log(checkBoolean("message", "mesSage"));
+
+// function checkForName(fullName, firstName) {
+//   const fullNameLower = fullName.toLowerCase();
+//   const firstNameLower = firstName.toLowerCase();
+//   const compare = fullNameLower.includes(firstNameLower);
+//   return compare;
+// }
+// console.log(checkForName("jacob vanilla", "Liza"));
+
+///////////////////////////// endsWith startsWith ///////////////////////
+
+// function checkFileExtension(fileName, ext) {
+//   if (fileName.endsWith(ext)) {
+//     return "File extension matches";
+//   } else {
+//     return "File extension does not match";
+//   }
+// }
+// console.log(checkFileExtension("project.html", "js"));
+
+//////////////////////////// indexOf() /////////////////////
+
+// function getFileName(file) {
+//   if (file.indexOf(".") > 0) {
+//     const fileName = file.indexOf(".");
+//     return file.slice(0, fileName);
+//   } else {
+//     return file.slice(0);
+//   }
+// }
+// console.log(getFileName("index.js"));
+
+// //////////////////////////// trim() ////////////////////////////
+// function createFileName(name, ext) {
+//   const noSpaceName = name.trim();
+//   const noSpaceExt = ext.trim();
+//   return noSpaceName + "." + noSpaceExt;
+// }
+
+// console.log(createFileName(" train   ", " js  "));
